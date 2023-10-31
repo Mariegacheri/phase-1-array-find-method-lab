bafa128d053ff47b2f03916bbc8b114590a5cdf4
marie <mariegacheri@gmail.com>
2023-10-31 20:39:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function superbowlWin(record) {
+    const winObject = record.find(entry => entry.result === "W");
+  
+    if (winObject) {
+      return winObject.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const record = [
+    { year: 1997, result: "W" },
+    { year: 1998, result: "L" },
+    { year: 2016, result: "L" },
+    { year: 2018, result: "W" },
+  ];
+  
+  const yearOfWin = superbowlWin(record); // This would return the year 1997.
+  
